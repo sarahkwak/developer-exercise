273a50dd21cf15b9db0fac8ac72078260a6eb6c3
Sarah Kwak <sarahkwak@hotmail.com>
2015-10-28 17:45:21 -0700

    answer for two challenges and rspec passed

--- exercise.rb
+++ exercise.rb
@@ -1,18 +1,38 @@
 class Exercise
 
-  # Assume that "str" is a sequence of words separated by spaces.
-  # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
-  # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    words = str.split(' ')
+    result = []
+    marklar = 'marklar'
+    words.each do |word|
+      if word.length > 4 
+        if word[-1] == word[/\W/]
+        result.push(marklar + word[-1])
+        elsif word[0] == word[/[A-Z]/]
+          result.push(marklar.capitalize)
+        else word[/[a-z]*/] == word
+        result.push(marklar)
+        end
+      else
+        result.push(word)
+      end
+    end
+    result.join(' ')
   end
 
-  # Return the sum of all even numbers in the Fibonacci sequence, up to
-  # the "nth" term in the sequence
-  # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
-  # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    return 2 if nth == 2
+    return "Number should be at least 3 to validate fibonacci sequence" if nth < 2
+    first = 1
+    second = 1
+    third = 2
+    result = [first, second, third]
+    (nth - 3).times do 
+      first = second
+      second = third
+      third = first + second
+      result.push(third)
+    end
+    final = result.map {|num| num if num % 2 == 0 }.compact.reduce(:+)
   end
-
 end
